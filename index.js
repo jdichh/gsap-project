1c9552337d33efe4742bbb06564b509e519a7b8a
jdichh <jpsdichoso21@gmail.com>
2023-08-18 11:37:58 +0800

    three js now integrated

--- index.js
+++ index.js
@@ -9,3 +9,29 @@ if (!WebGL.isWebGLAvailable()) {
     warningElement.appendChild(warning);
     warningElement.classList.add("webgl-warning-visible");
   }
+
+let watch;
+const container = document.querySelector(".canvas-container"); // Use ".canvas-container" for class selector
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.1, 1000); // Use container dimensions
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(container.clientWidth, container.clientHeight); // Use container dimensions
+container.appendChild(renderer.domElement); // Append renderer to container
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+function animate() {
+	requestAnimationFrame(animate);
+
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+
+	renderer.render(scene, camera);
+}
+
+animate();
